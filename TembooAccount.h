c033ef585c59d44bf2a0ecfd16aac5a18bf7b02f
sarabepu <sm.bejarano44@uniandes.edu.co>
2017-05-04 21:21:32 -0500

    Update TembooAccount.h

--- TembooAccount.h
+++ TembooAccount.h
@@ -18,4 +18,5 @@ and copy this content into it.
 The same TembooAccount.h file settings can be used for all Temboo SDK sketches.  
 Keeping your account information in a separate file means you can share the 
 main .ino file without worrying that you forgot to delete your credentials.
+Hola que hace
 */
