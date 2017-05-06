df69c179fbd891eeacf2c88e707dbc344e0db287
sarabepu <sm.bejarano44@uniandes.edu.co>
2017-05-05 22:46:21 -0500

    ultimo

--- prueba_2/prueba_2.ino
+++ prueba_2/prueba_2.ino
@@ -2,8 +2,6 @@
 #include <Temboo.h>
 #include "TembooAccount.h" // contains Temboo account information, as described below
 
-int calls = 1;   // Execution count, so this doesn't run forever
-int maxCalls = 10;   // Maximum number of times the Choreo should be executed
 const int sensorPin= A0;
 void setup() {
   Serial.begin(9600);
@@ -21,11 +19,9 @@ void loop() {
   Serial.print(celsius);
   Serial.println(" C");
   delay(1000);
-  if (celsius < 20 || celsius > 25) {
+  if (celsius < 20 ) {
 
-    
-    Serial.println("Running SendSMS - Run #" + String(calls++));
-    
+    Serial.println("Mandando mensaje");
     TembooChoreo SendSMSChoreo;
 
     // Invoke the Temboo client
@@ -40,7 +36,39 @@ void loop() {
     SendSMSChoreo.addInput("AuthToken", "5c582b1593093a71678c0cb02b236020");
     SendSMSChoreo.addInput("To", "+573138675183");
     SendSMSChoreo.addInput("From", "+17605306946");
-    SendSMSChoreo.addInput("Body", "hola cara de bola");
+    SendSMSChoreo.addInput("Body", "Muy frio!! " + String(celsius) + " C" + " Abrígate y concéntrate");
+    SendSMSChoreo.addInput("AccountSID", "AC87d462151b3882ddaa6ddae78c579843");
+    
+    // Identify the Choreo to run
+    SendSMSChoreo.setChoreo("/Library/Twilio/SMSMessages/SendSMS");
+    
+    // Run the Choreo; when results are available, print them to serial
+    SendSMSChoreo.run();
+    
+    while(SendSMSChoreo.available()) {
+      char c = SendSMSChoreo.read();
+      Serial.print(c);
+    }
+    SendSMSChoreo.close();
+  }
+  if (celsius > 25 ) {
+
+    Serial.println("Mandando mensaje");
+    TembooChoreo SendSMSChoreo;
+
+    // Invoke the Temboo client
+    SendSMSChoreo.begin();
+
+    // Set Temboo account credentials
+    SendSMSChoreo.setAccountName(TEMBOO_ACCOUNT);
+    SendSMSChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
+    SendSMSChoreo.setAppKey(TEMBOO_APP_KEY);
+    
+    // Set Choreo inputs
+    SendSMSChoreo.addInput("AuthToken", "5c582b1593093a71678c0cb02b236020");
+    SendSMSChoreo.addInput("To", "+573138675183");
+    SendSMSChoreo.addInput("From", "+17605306946");
+    SendSMSChoreo.addInput("Body", "Que calor!! " + String(celsius) + " C" + " Estamos bajando la temperatura. Relajate y estudia.");
     SendSMSChoreo.addInput("AccountSID", "AC87d462151b3882ddaa6ddae78c579843");
     
     // Identify the Choreo to run
@@ -56,6 +84,5 @@ void loop() {
     SendSMSChoreo.close();
   }
 
-  Serial.println("Waiting...");
-  delay(30000); // wait 30 seconds between SendSMS calls
+  delay(3000); // wait 30 seconds between SendSMS calls
 }
