2e610018075d7d0744e94890dd5c6a636c118023
sarabepu <sm.bejarano44@uniandes.edu.co>
2017-05-17 20:59:18 -0500

    final

---
+++ temperaturaWeb/temperaturaWeb.ino
@@ -0,0 +1,25 @@
+#include <Bridge.h>
+#include <BridgeServer.h>
+#include <BridgeClient.h> 
+BridgeServer server;
+
+void setup() {
+  Bridge.begin();
+  server.listenOnLocalhost();
+  server.begin();
+}
+void loop() {
+  BridgeClient client = server.accept();
+  if (client) {
+    String command = client.readString();
+    command.trim();
+    if (command == "temperature") {
+      int value = analogRead(A1);
+    float millivolts = (value / 1023.0) * 5000;
+    float celsius = millivolts / 10; 
+      client.print(celsius);
+    }
+    client.stop();
+  }
+  delay(50);
+}
